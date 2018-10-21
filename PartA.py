d9ff493d1879b8aa4d7524cad6ff65d33b5fe1df
Jason <37915983+liulhs@users.noreply.github.com>
2018-10-21 01:30:40 -0700

    2018/10/21 1:29am Second try

--- PartA.py
+++ PartA.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from _collections import defaultdict
+import string
 
 def ask_and_check():
     while(True):
@@ -13,17 +15,24 @@ def ask_and_check():
 
 def open_and_read(file_path:Path):
     try:
-        file_path.open()
+        file_open = file_path.open()
     except(Exception):
         print('Something went wrong while opening the file, Sorry...')
-        return
+    result = defaultdict(int)
+    for line in file_open:
+        for word in word_seperate(line):
+            if word.isalpha():
+                result[word] += 1
+    return result
     
         
 
 def word_seperate(target : str):
-    
-    return
+    table = str.maketrans({key: None for key in string.punctuation})
+    return target.strip().translate(table).split()
 
-ask_and_check()
-print('ended')
+file_path = ask_and_check()
+result = open_and_read(file_path)
+for i,j in result.items():
+    print('{}:{}'.format(i,j))
 
